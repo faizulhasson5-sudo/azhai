b4b458aa9f8f8ea98383333bf171b79d82ee9be5
faizulhasson5-sudo <faizulhasson5-sudo@users.noreply.github.com>
2026-07-28 00:06:54 +0500

    In-page book reader: Gutenberg books open in-reader with dictionary widget

--- build-books.js
+++ build-books.js
@@ -1087,7 +1087,7 @@ books.forEach(b => {
       '<span>' + esc(b.pages) + ' pages</span>' +
       '<span>' + esc(b.rating) + ' ★</span>' +
     '</div>' +
-    '<a href="' + esc(b.readUrl) + '" class="book-card-btn" target="_blank" rel="noopener">' + esc(b.readType) + ' →</a>' +
+    '<a href="' + (b.readUrl.indexOf('gutenberg.org')!==-1 ? '/books/reader.html?url='+encodeURIComponent(b.readUrl)+'&title='+encodeURIComponent(b.title)+'&author='+encodeURIComponent(b.author)+'&genre='+encodeURIComponent(b.genre) : esc(b.readUrl)) + '" class="book-card-btn"' + (b.readUrl.indexOf('gutenberg.org')!==-1 ? '' : ' target="_blank" rel="noopener"') + '>' + esc(b.readType) + ' →</a>' +
   '</div>\n';
 });
 
